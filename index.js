daf2eb8d096a91464c0a1e2c92919ddfedf87c57
Akin Sel Coskun <akn_coskun@outlook.com>
2022-12-28 19:26:22 +0300

    f

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import orderRoute from './routes/orders.js'
 
 env.config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // db connection
 const connect = async ()=>{
